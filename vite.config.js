059bac4187beceda2bcd2252c93100f50ae7d6bd
amaranwrites <raghavvicky1@gmail.com>
2025-01-10 12:02:02 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/home',
   plugins: [react()],
   build: {
     outDir: 'build'
